b357501df9602c04727f26021e1e7555eaba38d0
Caleb Hiebert <calebhiebert@users.noreply.github.com>
2021-09-02 10:27:36 -0500

    http col

---
+++ ui/src/components/admin/ColumnViewHTTPResponseCode.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <b-tag>{{ row[col.field] }}</b-tag>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ColumnViewHTTPResponseCode",
+
+    props: {
+        row: {
+            type: Object,
+            required: true,
+        },
+
+        col: {
+            type: Object,
+            required: true,
+        },
+
+        p: {
+            type: Object,
+            required: true,
+        }
+    },
+
+    computed: {
+        tagType() {
+            const data = this.row[this.col.field];
+
+            if (data >= 200 && data < 300) {
+                return "is-success";
+            } else {
+                return "is-light"
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
